3aec8bbd95db751d67cf8d1034816c05ebf51e3d
HFintan <fintan.hegarty@gmail.com>
2015-04-10 04:59:01 +0100

    Update sparsifyMat.m

--- sparsifyMat.m
+++ sparsifyMat.m
@@ -1,4 +1,4 @@
-% This function takes a dense matrix DENSE_MAT and returns a sparse matrix
+% This function takes a dense matrix DENSE_MAT and returns a normalised sparse matrix
 % SparseMat where PROPORTION of the entries in each column of SparseMat 
 % are equal to the corresponding entries in DENSE_MAT and all other entries
 % are zero. This is done by taking the pointwise product with a random
@@ -9,7 +9,12 @@ function SparseMat = sparsifyMat(DENSE_MAT, PROPORTION);
         if PROPORTION==1
             SparseMat=DENSE_MAT;
         else
-        [Rows, Cols] = size(DENSE_MAT);
-        CoefficientMat = sparsifyingMat(Rows, Cols, PROPORTION);
-        SparseMat = DENSE_MAT .* CoefficientMat;             % Pointwise product.
+            [Rows, Cols] = size(DENSE_MAT);
+            CoefficientMat = sparsifyingMat(Rows, Cols, PROPORTION);
+            SparseMat = DENSE_MAT .* CoefficientMat;             % Pointwise product.
         end
+Cols=size(SparseMat,2);
+for c=1:Cols
+    SparseMat(:,c)=SparseMat(:,c)/norm(SparseMat(:,c));
+end
+end
