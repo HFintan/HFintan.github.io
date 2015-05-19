b06322c9789f79ffccd07b3a0ffbf320abb6c530
HFintan <fintan.hegarty@gmail.com>
2015-05-19 08:22:18 +0100

    Update testOneMat.m

--- testOneMat.m
+++ testOneMat.m
@@ -1,24 +1,26 @@
-function OneMatIter = testOneMat(ROWS, COLS, MAT_TYPE, SPARSITY_LIST, VEC_SPARSITY)
+function OneMatIter = testOneMat(ROWS, COLS, MAT_TYPE, SPARSITY_LIST, VEC_SPARSITY,ALG)
 % Generates a ROWS by COLS matrix DenseMat
 % Creates a number of sparsified versions of DenseMat and for each one,
 % generates a sparse vector Vec and tests whether linprog (or the OMP or
 % CoSaMP algorithms, if selected) successfully
 % recovers it. The output is a 0,1-vector, indicating the success or
-% failure of the test for of the various sparsifications.
+% failure of the test for each of the various sparsifications.
 [T,DenseMat]=evalc('generateMat(MAT_TYPE, ROWS, COLS)');
 ResultsVec=zeros(1, size(SPARSITY_LIST,2));
+Vec=randomKSparseVector(VEC_SPARSITY, COLS);
+LowVecVals=zeros(COLS, 1);
+UppVecVals=ones(COLS, 1);
+ObjectiveFunction=ones(COLS, 1);
 for i=1:size(SPARSITY_LIST, 2)
     SparseMat=sparsifyMat(DenseMat, SPARSITY_LIST(i));
-    Vec=randomKSparseVector(VEC_SPARSITY, COLS);
-    LowVecVals=zeros(COLS, 1);
-    UppVecVals=ones(COLS, 1);
-    ObjectiveFunction=ones(COLS, 1);
+    if ALG=='L'
     [T,GuessVec]=evalc('linprog(ObjectiveFunction, [], [], SparseMat, SparseMat*Vec, LowVecVals, UppVecVals)');
-        % The [],[] here indicate that we are not seeking inequalities.
-%    [T,GuessVec]=evalc('OMP(SparseMat, SparseMat*Vec, 10^-6, [])');
-%tic
-%    [T,GuessVec]=evalc('CoSaMP(SparseMat,SparseMat*Vec,VEC_SPARSITY,[])');
-%toc
+    % The [],[] here indicate that we are not seeking inequalities.
+    elseif ALG=='O'
+        [T,GuessVec]=evalc('OMP(SparseMat, SparseMat*Vec, VEC_SPARSITY, [])');
+    elseif ALG=='C'
+    [T,GuessVec]=evalc('CoSaMP(SparseMat,SparseMat*Vec,VEC_SPARSITY,[])');
+    end
     ResultsVec(i)=((norm(Vec-GuessVec, 1))<10^-6);
 
 end
